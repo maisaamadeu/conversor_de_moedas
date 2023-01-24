5122fdd06e1ae434a509de9c1b5ccc4854f446b2
Maísa Amadeu <maisa.antonella.amadeu@gmail.com>
2023-01-24 14:55:36 -0300

    preparando o ambiente pro request

---
+++ lib/pages/conversor_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+
+const request = 'https://api.hgbrasil.com/finance?key=60892ac4';
+
+class ConversorPage extends StatelessWidget {
+  const ConversorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
